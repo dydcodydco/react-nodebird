43014d44ae46ee3dc360722aef0c50599c5b219a
asd0905 <dydcodydco@daum.net>
2024-04-28 05:06:08 +0900

    feat: section3 redux-saga연동하기 - redux-thunk 이해하기

--- prepare/front/store/configurStore.js
+++ prepare/front/store/configurStore.js
@@ -2,14 +2,25 @@ import { createWrapper } from "next-redux-wrapper";
 import { configureStore } from "@reduxjs/toolkit";
 import reducer from "../reducers"; // reducers/index의 rootReducer불러온 것
 
+// redux-thunk를 참조해서 만든 미들웨어
+const loggerMiddleware =
+	({ dispatch, getState }) =>
+	(next) =>
+	(action) => {
+		// console.log(action);
+		return next(action);
+	};
+
 // configureStore 자체에서 Redux DevTools 설정을 자동으로 처리합니다.
 const makeStore = () =>
 	// configureStore: store 를 생성
 	configureStore({
 		reducer, // 리듀서 모듈들이 합쳐진 루트 리듀서
-		middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+		middleware: (getDefaultMiddleware) =>
+			getDefaultMiddleware().concat(loggerMiddleware),
 		// redux-toolkit 은 devTools 등의 미들웨어들을 기본적으로 제공
 		// 추가 미들웨어나 enhancers가 필요하다면 여기에 포함시킬 수 있습니다.
+		devTools: true,
 	});
 
 const wrapper = createWrapper(makeStore, {
