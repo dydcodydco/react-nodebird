74b33db95d0225b1a094137cf57adb37ee2422db
asd0905 <dydcodydco@daum.net>
2024-05-09 08:44:28 +0900

    Update: cors origin > front 서버 추가

--- prepare/back/app.js
+++ prepare/back/app.js
@@ -1,50 +1,50 @@
 // node에서는 import / export 안쓰고 require / module.exports 사용
-const express = require("express");
-const cors = require("cors");
-const session = require("express-session");
-const cookieParser = require("cookie-parser");
-const passport = require("passport");
-const dotenv = require("dotenv");
-const morgan = require("morgan");
-const path = require("path");
-const hpp = require("hpp");
-const helmet = require("helmet");
+const express = require('express');
+const cors = require('cors');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const passport = require('passport');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const path = require('path');
+const hpp = require('hpp');
+const helmet = require('helmet');
 
-const postRouter = require("./routes/post");
-const postsRouter = require("./routes/posts");
-const userRouter = require("./routes/user");
-const hashtahRouter = require("./routes/hashtag");
-const db = require("./models"); // sequelize에서 model 모두 등록 // express에서 그 sequelize를 등록해야 한다.
-const passportConfig = require("./passport");
+const postRouter = require('./routes/post');
+const postsRouter = require('./routes/posts');
+const userRouter = require('./routes/user');
+const hashtahRouter = require('./routes/hashtag');
+const db = require('./models'); // sequelize에서 model 모두 등록 // express에서 그 sequelize를 등록해야 한다.
+const passportConfig = require('./passport');
 
 dotenv.config();
 const app = express();
 db.sequelize
 	.sync()
 	.then(() => {
-		console.log("db연결 성공!");
+		console.log('db연결 성공!');
 	})
 	.catch(console.error);
 passportConfig();
 
 // 운영용 빌드
-if (process.env.NOD_ENV === "production") {
-	app.use(morgan("combined")); // 로그볼 수 있게 해주는 것
+if (process.env.NOD_ENV === 'production') {
+	app.use(morgan('combined')); // 로그볼 수 있게 해주는 것
 	// 보안에 도움되는 패키지들
 	app.use(hpp());
 	app.use(helmet());
 } else {
-	app.use(morgan("dev")); // 로그볼 수 있게 해주는 것
+	app.use(morgan('dev')); // 로그볼 수 있게 해주는 것
 }
 // app.use(cors()) -> 모든 요청에 다 res.setHeader("Access-Control-Allow-Origin", "*") 설정 넣어주는 것
 app.use(
 	cors({
-		origin: ["http://localhost:3000", "nodebird.com"], // true or * // access-control-allow-origin가 true된다. --> 다른 도메인끼리 api 요청
+		origin: ['http://localhost:3000', 'nodebird.com', '13.125.119.94'], // true or * // access-control-allow-origin가 true된다. --> 다른 도메인끼리 api 요청
 		credentials: true, // access-control-allow-credential가 true된다. --> 다른 도메인끼리 쿠키 전달
 	})
 );
 // __dirname -> 현재 폴더, 이 폴더 안에 uploads폴더 경로를 합쳐준다. // 운영체제 차이때문에 이렇게 경로를 설정해줌
-app.use("/", express.static(path.join(__dirname, "uploads")));
+app.use('/', express.static(path.join(__dirname, 'uploads')));
 // req.body를 사용하기 위해 라우터 연결 이전에 아래 두 미들웨어(express.json, express.urlencoded) 적용해야한다.
 // use는 express 서버에다가 무언갈 장착한다는 뜻
 // 아래 두 코드가 프론트에서 받은 데이터를 req.body에 넣어주는 역할을 한다.
@@ -67,11 +67,11 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // api의 앞쪽 중복되는 부분을 prefix(접두어)로 뽑아내서 첫번째 인자에 넣을 수 있다. ('/post')
-app.use("/post", postRouter);
-app.use("/posts", postsRouter);
-app.use("/user", userRouter);
-app.use("/hashtag", hashtahRouter);
+app.use('/post', postRouter);
+app.use('/posts', postsRouter);
+app.use('/user', userRouter);
+app.use('/hashtag', hashtahRouter);
 
 app.listen(80, () => {
-	console.log("서버 실행 중");
+	console.log('서버 실행 중');
 });
